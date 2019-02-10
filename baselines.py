c617e11d8b2cc53d77941238e9ec3e8bab6593e1
Nikita Churikov <nikita@chur.ru>
2019-02-10 20:24:10 +0300

    Started encoder decoder architecture

--- baselines.py
+++ baselines.py
@@ -4,6 +4,7 @@ from typing import Callable, Optional
 from gensim.summarization.summarizer import summarize
 from gensim.summarization.textcleaner import split_sentences
 from tqdm import tqdm
+from torch import nn
 
 from loader import json_iterator
 from preprocessing import BasicHtmlPreprocessor
@@ -65,3 +66,41 @@ class ExtractFirstFullSentence(GensimSummarizer):
             return sentences[1]
         else:
             return sentences[0]
+
+
+class Encoder(nn.Module):
+
+    def __init__(self, embedding: nn.Embedding, lstm_n_layers=4, lstm_hidden_size=512, embedding_dim=300,
+                 lstm_batch_first=True, lstm_bidirectional=True, lstm_dropout=0.5, embed_dropout=0.5):
+        super().__init__()
+        self.hidden_size = lstm_hidden_size
+        self.n_layers = lstm_n_layers
+        self.embedding_dim = embedding_dim
+        self.embedding = embedding
+
+        self.dropout = nn.Dropout(embed_dropout)
+        self.lstm = nn.LSTM(input_size=self.embedding_dim, hidden_size=self.hidden_size,
+                            num_layers=self.n_layers, batch_first=lstm_batch_first,
+                            bidirectional=lstm_bidirectional, dropout=lstm_dropout)
+
+    def forward(self, input):
+        embed = self.dropout(self.embedding(input))
+
+        _, (h, c) = self.lstm(embed)
+
+        return (h, c)
+
+
+class Decoder(nn.Module):
+
+    def __init__(self, embedding: nn.Embedding):
+        super().__init__()
+        self.embedding = embedding
+
+
+class Seq2SeqSummarizer(nn.Module):
+    """Implementation of http://anlp.jp/proceedings/annual_meeting/2018/pdf_dir/A1-2.pdf with bpe"""
+    def __init__(self, encoder, decoder):
+        super().__init__()
+        self.decoder = decoder
+        self.encoder = encoder
