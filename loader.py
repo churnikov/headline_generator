e272341cf7647404298ec3337f0d961a87364758
Nikita Churikov <nikita@chur.ru>
2019-02-14 23:19:56 +0300

    created translator from jsons to csvs

--- loader.py
+++ loader.py
@@ -1,4 +1,5 @@
 import argparse
+import csv
 import gzip
 import json
 import os
@@ -52,11 +53,33 @@ def json_iterator(file_name=os.path.join(CURRENT_PATH, 'ria.json.gz')):
                 yield json.loads(l)
 
 
+def jsons2csvs(file_name: str):
+    out_fn = file_name.split('.')[0] + '.csv'
+    with open(out_fn, 'w') as f:
+        writer = csv.writer(f)
+        writer.writerow(['text', 'title'])
+        for jsn in tqdm(json_iterator(file_name)):
+            writer.writerow([jsn['text'], jsn['title']])
+
+
+def __jsons2csvs(dataset_size, save_path):
+    if dataset_size == 'all':
+        __jsons2csvs('20', save_path)
+        __jsons2csvs('1k', save_path)
+        __jsons2csvs('full', save_path)
+    else:
+        file_name = os.path.join(save_path, (f'ria_{dataset_size}.json' if dataset_size != 'full' else 'ria.json.gz'))
+        jsons2csvs(file_name)
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--dataset_size', default='20')
     parser.add_argument('--save_path', default='data/dataset')
+    parser.add_argument('--make_csvs', default=True)
 
     args = parser.parse_args()
 
     download_dataset(args.dataset_size, args.save_path)
+    if args.make_csvs:
+        __jsons2csvs(args.dataset_size, args.save_path)
