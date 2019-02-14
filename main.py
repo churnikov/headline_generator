a0946bf18d911dd5714d98d48fe77df59de30675
Nikita Churikov <nikita@chur.ru>
2019-02-14 23:35:46 +0300

    Add train and evaluate functions

--- main.py
+++ main.py
@@ -8,6 +8,83 @@ import torch
 from baselines import *
 from preprocessing import *
 
+
+def train(model, train_data, optimizer, criterion, clip, device, teacher_forcing_ratio):
+    model.train()
+
+    epoch_loss = 0
+    i = 1
+
+    with tqdm(bar_format='{postfix[0]} {postfix[3][iter]}/{postfix[2]} {postfix[1]}: {postfix[1][loss]}',
+              postfix=['Training iter:', 'Loss', dict(loss=0, iter=0)]) as t:
+        for i, (x_train, y_train) in enumerate(train_data):
+            optimizer.zero_grad()
+
+            x_train, y_train = x_train.to(device), y_train.to(device)
+
+            output = model.forward(x_train, y_train, teacher_forcing_ratio)
+
+            y_true = y_train[1:].view(-1)
+            loss = criterion(output[1:].view(-1, output.shape[2]), y_true)
+            loss.backward()
+
+            torch.nn.utils.clip_grad_norm_(model.parameters(), clip)
+
+            optimizer.step()
+
+            epoch_loss += loss.item()
+
+            t.postfix[3]['loss'] = loss.item()
+            t.postfix[3]['iter'] = i
+            t.update()
+
+    return epoch_loss / i
+
+
+def evaluate(model, validation_data, criterion, device):
+    model.eval()
+
+    epoch_loss = 0
+    i = 1
+
+    with torch.no_grad():
+        for i, (x_val, y_val) in tqdm(enumerate(validation_data), desc='Validating'):
+
+            y_val, y_val = y_val.to(device), y_val.to(device)
+
+            y_true = y_val[1:].view(-1)
+
+            output = model.forward(x_val, y_val, 0)
+
+            loss = criterion(output[1:].view(-1, output.shape[2]), y_true)
+
+            epoch_loss += loss.item()
+
+    return epoch_loss / i
+
+
+def predict(model, x, max_len, end_symbol, id2word):
+    model.eval()
+
+    with torch.no_grad():
+        _, hidden = model.encoder(x)
+        inp = x[0, :]
+
+        symbol = ''
+        output = []
+
+        while symbol != end_symbol and len(output) < max_len:
+            out, hidden = model.decoder(inp, hidden)
+
+            idx = out.max(1)[1]
+            symbol = id2word[idx.item()]
+
+            output.append(symbol)
+            inp = idx
+
+    return ''.join(output)
+
+
 parser = argparse.ArgumentParser()
 parser.add_argument('--config_path')
 
@@ -66,7 +143,10 @@ if model_name == 'Seq2SeqSummarizer':
     else:
         raise NotImplementedError(f'Encoder {config["model"]["encoder"]["name"]} not yet implemented')
 
-    summarizer = Seq2SeqSummarizer(encoder, decoder, device=device)
+    embedding.to(device)
+    encoder.to(device)
+    decoder.to(device)
+    summarizer = Seq2SeqSummarizer(encoder, decoder, device=device).to(device)
 
 
 else:
