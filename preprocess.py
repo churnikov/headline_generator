532473f43b33389321c080f52d48ead62f058b50
Nikita Churikov <nikita@chur.ru>
2019-02-16 22:49:00 +0300

    Add ability to create test set

--- preprocess.py
+++ preprocess.py
@@ -8,13 +8,16 @@ from preprocessing import BasicHtmlPreprocessor
 parser = argparse.ArgumentParser()
 parser.add_argument('--preprocessor_name', default='BasicHtmlPreprocessor')
 parser.add_argument('-i')
-parser.add_argument('-o')
+parser.add_argument('--train_output', required=True)
+parser.add_argument('--test_output')
+parser.add_argument('--test_size', default=0)
 
 args = parser.parse_args()
 
 prepr_name: str = args.preprocessor_name
 input_file: str = args.i
-output_file: str = args.o
+train_output_file: str = args.train_output
+test_size: int = args.test_size
 
 if prepr_name == 'BasicHtmlPreprocessor':
     preprocessor = BasicHtmlPreprocessor()
@@ -22,13 +25,26 @@ else:
     raise NotImplementedError('This preprocessor is not implemented')
 
 
-with open(output_file, 'w') as out, open(input_file, 'r') as inp:
+with open(train_output_file, 'w') as train_out,  open(input_file, 'r') as inp:
     reader = csv.reader(inp)
-    writer = csv.writer(out)
+    train_writer = csv.writer(train_out)
 
-    writer.writerow(next(reader))
+    header = next(reader)
+
+    if test_size:
+        test_output_file: str = args.test_output
+        test_out = open(test_output_file, 'w')
+        test_writer = csv.writer(test_out)
+        test_writer.writerow(header)
+
+        for i, (text, title) in tqdm(enumerate(reader)):
+            test_writer.writerow([preprocessor.transform(text), title.lower()])
+            if i == test_size:
+                break
+
+    train_writer.writerow(header)
 
     for text, title in tqdm(reader):
-        writer.writerow([preprocessor.transform(text), title.lower()])
+        train_writer.writerow([preprocessor.transform(text), title.lower()])
 
 
